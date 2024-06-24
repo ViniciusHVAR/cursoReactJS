abe535fd94d6cc8c4a8c748bb70fc693d1582f9b
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-06-24 14:19:11 -0300

    new clean repo

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export const RepositoryItem = () => {
+    return (
+        <li>
+            <strong>{repositoryName}</strong>
+            <p>Forms in React</p>
+
+            <a href="">
+                Acessar Repositório
+            </a>
+        </li>
+    )
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = 'unform2';
+
+export const RepositoryList = () => {
+    return (
+        <section className = "repository-list">
+            <h1>Lista de Repositórios</h1>
+            
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
